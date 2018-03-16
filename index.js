9081c727d8bc849bb67fd8d11feaf1691a0e9b68
jafow <jared.a.fowler@gmail.com>
2018-03-16 14:09:49 -0700

    setup memberProcess class; generate nodeid

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const protobuf = require('protocol-buffers')
 const dgram = require('dgram')
-const EventEmitter = require('events')
+const EventEmitter = require('events').EventEmitter
+const randombytes = require('randombytes')
 
 class MemberProcess extends EventEmitter {
+  constructor (opts) {
+    super()
+    var _opts = opts || {}
+    this.nodes = []
+    this.heartbeat = 0
+    this.idLength = opts.idLength || 20
+    this.id = _opts.id || makeId()
 
+    async function makeId () {
+      return Buffer.from(randombytes(this.idLength)).toString('hex')
+    }
+  }
+
+  queryAll (message) {
+    // query all nodes in list to get their updated memberlist
+
+  }
 }
 
 module.exports = MemberProcess
