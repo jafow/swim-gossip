82a538e4b0da900853bb9f983f1ff887b63e835d
jafow <jared.a.fowler@gmail.com>
2018-03-16 00:11:05 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const protobuf = require('protocol-buffers')
+const dgram = require('dgram')
+const EventEmitter = require('events')
+
+class MemberProcess extends EventEmitter {
+
+}
+
+module.exports = MemberProcess
