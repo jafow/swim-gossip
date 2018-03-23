5410d241a52b53d4ecf6ec6c5f313b5589f6757d
jafow <jared.a.fowler@gmail.com>
2018-03-23 14:08:52 -0700

    Create a socket, bind it and handle messages

--- index.js
+++ index.js
@@ -6,15 +6,54 @@ const randombytes = require('randombytes')
 class MemberProcess extends EventEmitter {
   constructor (opts) {
     super()
-    var _opts = opts || {}
+    var _this = this
+    var _opts = opts || {type: 'udp6', lookup: _this.lookup}
+    var membershipTable = {}
     this.nodes = []
     this.heartbeat = 0
     this.idLength = opts.idLength || 20
     this.id = _opts.id || makeId()
-    this.port = opts.port || 0;
+    this.port = opts.port || 0
+    this.socket = dgram.createSocket(opts, handleMessage)
+    this.socket.on('message', onmessage)
 
     async function makeId () {
       return Buffer.from(randombytes(this.idLength)).toString('hex')
+    }
+
+    function onmessage (msg, remoteInfo) {
+      var nodeId = remoteInfo.address.toString() + remoteInfo.port.toString()
+      if (!membershipTable[nodeId]) {
+        // new node; add it to list, update list, and ack back
+        var newNode = {
+          id: nodeId,
+          msg: msg,
+          addr: remoteInfo.address,
+          port: remoteInfo.port,
+          msgSize: remoteInfo.size
+        }
+        addNode(newNode)
+      }
+      updateList(msg, remoteInfo)
+      var ack = composeMsg(msg, remoteInfo)
+      this.socket.send(ack, 0, ack.length, remoteInfo.port, remoteInfo.address, onMessageSend)
+    }
+
+    function composeMsg (mgs, rinfo) {
+
+    }
+
+    function updateList (msg, rinfo) {
+
+    }
+
+    function addNode (node) {
+      membershipTable[node.id] = { msg: node.msg, addr: node.addr, port: node.port }
+      return membershipTable
+    }
+
+    function onMessageSend () {
+      console.log('message sent')
     }
   }
 
@@ -22,20 +61,20 @@ class MemberProcess extends EventEmitter {
     // query all nodes in list to get their updated memberlist
 
   }
-  sendPing(node) {
+  sendPing (node) {
 
   }
-  lookup(msg) {
+  lookup (msg) {
 
   }
-  listen() {
-    var opts = {type: 'udp4', lookup: lookup}
-    this.socket = dgram.createSocket(opts, handleMessage)
+  listen () {
+    this.socket.bind(this.port, this.address)
+    this.socket.on('error', (err) => { console.error('Error binding socket: ', err) })
   }
+}
 
-  function handleMessage(msg) {
-    console.log('got mssg: ', msg.toString())
-  }
+function handleMessage (msg) {
+  console.log('got mssg: ', msg.toString())
 }
 
 module.exports = MemberProcess
